2848571938d8bc524b795c8f992e9efb9d495b30
Gammouha <sarah_gmiha@hotmail.fr>
2019-04-21 21:51:33 +0100

    Produit

--- splite/Categoriescrud/views/supprimerCategorie.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "../core/categorie.php";
-$categorieC=new CategorieC();
-if (isset($_POST["numcat"])){
-	$categorieC->supprimerCategorie($_POST["numcat"]);
-	header('Location: afficherCategorie.php');
-}
-
-?>
